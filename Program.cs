1333428aac2578f8b8fc33b7ecb508716997c810
matheushribeiro545 <matheusribeiro545@gmail.com>
2022-12-05 19:25:34 -0300

    Criaçāo de menu interativo

--- Program.cs
+++ Program.cs
@@ -1,35 +1,45 @@
 ﻿using desenv_dotnet.Models;
 
-// Estrutura de repetiçāo
+// Menu interativo
 
-// int numero = 5;
+string? opcao;
+bool exibirMenu = true;
 
-//for (declarar variável; até quando; enquanto o contador for considerado)
-// for(int contador = 0; contador <= 10; contador++)
-// {
-//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-// }
-
-// int numero = 10;
-// int contador = 0;
-
-// while (contador <= 10)
-// {
-//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-//     contador++; // Para nāo cair em loop infinito
-
-//     // break; equivalente ao return do SQL server
-// }
-
-int soma = 0, numero = 0;
-
-do 
+while(exibirMenu)
 {
-    Console.WriteLine("Digite um número (0 para parar)");
-    numero = Convert.ToInt32(Console.ReadLine());
+    Console.Clear(); // Limpa o console
+    Console.WriteLine("Digite a sua opçāo:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
 
-    soma += numero;
+    opcao = Console.ReadLine();
 
-} while (numero != 0);
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
 
-Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+        case "2":
+            Console.WriteLine("Buscar cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            //Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Opçāo inválida");
+            break;
+    }
+}
+
+Console.WriteLine("O programa se encerrou");
